513420357be88075a334142f5af92b2f93689f66
JeannieFallon <jeanniefallon@gmail.com>
2019-07-07 08:00:46 -0400

    move main control flow to BaseController

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -7,5 +7,27 @@ namespace Ciphers.Controllers
 {
     public class BaseController
     {
+        public static void Index()
+        {
+            string plainTxt, keyWord;
+            int key;
+
+            Console.WriteLine("Enter plaintext:");
+            plainTxt = Console.ReadLine();
+
+            Console.WriteLine("Enter a key value for the Caesar cipher:");
+            key = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a key word for the Vigenere cipher:");
+            keyWord = Console.ReadLine();
+
+            Rot13Text rot13Text = new Rot13Text(plainTxt);
+            CaesarText caesarText = new CaesarText(plainTxt, key);
+            VigenereText vigenere = new VigenereText(plainTxt, keyWord);
+
+            Console.WriteLine(string.Format("\nROT13:\n" + "test"));
+            Console.WriteLine(string.Format("\nCaesar:\n" + "test"));
+            Console.WriteLine(string.Format("\nVigenere:\n" + "test"));
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Ciphers.Models;
-using System;
+﻿using Ciphers.Controllers;
 
 namespace Ciphers
 {
@@ -7,27 +6,7 @@ namespace Ciphers
     {
         static void Main(string[] args)
         {
-            string plainTxt, keyWord;
-            int key;
-
-            Console.WriteLine("Enter plaintext:");
-            plainTxt = Console.ReadLine();
-
-            Console.WriteLine("Enter a key value for the Caesar cipher:");
-            key = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter a key word for the Vigenere cipher:");
-            keyWord = Console.ReadLine();
-
-            Rot13Text rot13Text = new Rot13Text(plainTxt);
-            CaesarText caesarText = new CaesarText(plainTxt, key);
-            VigenereText vigenere = new VigenereText(plainTxt, keyWord);
-
-            Console.WriteLine(string.Format("\nROT13:\n" + "test"));
-
-            Console.WriteLine(string.Format("\nCaesar:\n" + "test"));
-
-            Console.WriteLine(string.Format("\nVigenere:\n" + "test"));
+            BaseController.Index();
         }
     }
 }
